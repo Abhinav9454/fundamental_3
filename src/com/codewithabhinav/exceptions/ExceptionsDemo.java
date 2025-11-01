dc913f4f61a34d605ae2b254fe0169ee6d981707
Abhinav <abhinavnagvanshi26@gmail.com>
2025-11-02 00:49:19 +0530

    try and Catch

--- src/com/codewithabhinav/exceptions/ExceptionsDemo.java
+++ src/com/codewithabhinav/exceptions/ExceptionsDemo.java
@@ -1,9 +1,15 @@
 package com.codewithabhinav.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ExceptionsDemo {
     public static void show(){
-        var reader = new FileReader("file.txt");
+        try{
+            var reader = new FileReader("file.txt");
+        }catch (FileNotFoundException ex){
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("file opened");
     }
 }
