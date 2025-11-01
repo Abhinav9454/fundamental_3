70f29d66d6904877b71338f865e1e597d85c1eaa
Abhinav <abhinavnagvanshi26@gmail.com>
2025-11-01 19:47:55 +0530

    Null Exception

---
+++ src/com/codewithabhinav/Main.java
@@ -0,0 +1,9 @@
+package com.codewithabhinav;
+
+import com.codewithabhinav.exceptions.ExceptionsDemo;
+
+public class Main  {
+    public static void main(String[] args) {
+        ExceptionsDemo.show();
+    }
+}
