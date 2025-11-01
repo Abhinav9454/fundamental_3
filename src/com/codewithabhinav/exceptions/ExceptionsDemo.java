f4264bc3b07d815d2868054bcc8d1a783451fa8f
Abhinav <abhinavnagvanshi26@gmail.com>
2025-11-01 20:03:50 +0530

    FileNotFoundException

--- src/com/codewithabhinav/exceptions/ExceptionsDemo.java
+++ src/com/codewithabhinav/exceptions/ExceptionsDemo.java
@@ -1,10 +1,9 @@
 package com.codewithabhinav.exceptions;
 
+import java.io.FileReader;
+
 public class ExceptionsDemo {
     public static void show(){
-        sayHello(null);
-    }
-    public static void sayHello(String name){
-        System.out.println(name.toUpperCase());
+        var reader = new FileReader("file.txt");
     }
 }
