c6420f23ccd59bd4f0ef27a96b32133be0bfe44f
Abhinav <abhinavnagvanshi26@gmail.com>
2025-11-02 12:18:53 +0530

    finally block

--- src/com/codewithabhinav/exceptions/ExceptionsDemo.java
+++ src/com/codewithabhinav/exceptions/ExceptionsDemo.java
@@ -1,20 +1,28 @@
 package com.codewithabhinav.exceptions;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show(){
+        FileReader reader = null;
+
         try {
-            var reader = new FileReader("file.txt");
+            reader = new FileReader("file.txt");
             var value = reader.read();
-            new SimpleDateFormat().parse("");
         }
-        catch (IOException | ParseException e) {
+        catch (IOException e) {
             System.out.println("could not read data");
+        }
+        finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+//                    throw new RuntimeException(e);
+                }
+            }
         }
     }
 }
