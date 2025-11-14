53a95d8f9f781e042a6cb258f5a3561a3dbe80d9
Abhinav <abhinavnagvanshi26@gmail.com>
2025-11-14 19:28:15 +0530

    A Poor Solution Of Generic

--- src/com/codewithabhinav/Main.java
+++ src/com/codewithabhinav/Main.java
@@ -1,10 +1,15 @@
 package com.codewithabhinav;
 
 import com.codewithabhinav.generics.List;
+import com.codewithabhinav.generics.User;
 
 public class Main  {
     public static void main(String[] args) {
         var list = new List();
-        list.add(1);
+        list.add(Integer.valueOf(1));
+        list.add("1");
+        list.add(new User());
+
+        int number = (int)list.get(1);
     }
 }
