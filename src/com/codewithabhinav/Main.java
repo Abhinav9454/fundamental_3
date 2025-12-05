7d7947f8777e9be56615dbd0b4bbc238ebb532c4
Abhinav <abhinavnagvanshi26@gmail.com>
2025-12-05 20:03:42 +0530

    Constraints

--- src/com/codewithabhinav/Main.java
+++ src/com/codewithabhinav/Main.java
@@ -5,8 +5,6 @@ import com.codewithabhinav.generics.User;
 
 public class Main  {
     public static void main(String[] args) {
-        GenericList<Integer> numbers = new GenericList<>();
-        numbers.add(1); // boxing
-        int number = numbers.get(0);  // unboxing
+        new GenericList<User>();
     }
 }
--- src/com/codewithabhinav/generics/User.java
+++ src/com/codewithabhinav/generics/User.java
@@ -1,5 +1,5 @@
 package com.codewithabhinav.generics;
 
-public class User {
+public class User implements Comparable{
 
 }
